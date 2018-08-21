748372132128f634c7060ec5014afd5f4da07aea
George Romero <george.romeroo93@gmail.com>
2018-08-21 14:13:26 -0400

    display events as clickable

--- app/javascript/bundles/Calendar/components/Calendar.jsx
+++ app/javascript/bundles/Calendar/components/Calendar.jsx
@@ -1,15 +1,47 @@
 import React from "react";
 import dateFns from "date-fns";
+import axios from "axios";
 
 class Calendar extends React.Component {
   state = {
     currentMonth: new Date(),
-    selectedDate: new Date()
+    selectedDate: new Date(),
+    events: {}
   };
+
+  componentDidMount() {
+    const { currentMonth, selectedDate } = this.state;
+    const monthStart = dateFns.startOfMonth(currentMonth);
+    const monthEnd = dateFns.endOfMonth(monthStart);
+    const startDate = dateFns.startOfWeek(monthStart);
+    const endDate = dateFns.endOfWeek(monthEnd);
+    const dateFormat = "YYYY-MM-DD";
+    const formattedStartDate = dateFns.format(startDate, dateFormat);
+    const formattedEndDate = dateFns.format(endDate, dateFormat);
+    axios
+      .get(
+        `/events.json?start_date=${formattedStartDate}&end_date=${formattedEndDate}`
+      )
+      .then(response => {
+        this.setState({ events: response.data });
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
+  }
+
+  render() {
+    return (
+      <div className="calendar">
+        {this.renderHeader()}
+        {this.renderDays()}
+        {this.renderCells()}
+      </div>
+    );
+  }
 
   renderHeader() {
     const dateFormat = "MMMM YYYY";
-
     return (
       <div className="header row flex-middle">
         <div className="col col-start">
@@ -30,9 +62,7 @@ class Calendar extends React.Component {
   renderDays() {
     const dateFormat = "dddd";
     const days = [];
-
     let startDate = dateFns.startOfWeek(this.state.currentMonth);
-
     for (let i = 0; i < 7; i++) {
       days.push(
         <div className="col col-center" key={i}>
@@ -40,27 +70,27 @@ class Calendar extends React.Component {
         </div>
       );
     }
-
     return <div className="days row">{days}</div>;
   }
 
   renderCells() {
-    const { currentMonth, selectedDate } = this.state;
+    const { currentMonth, selectedDate, events } = this.state;
     const monthStart = dateFns.startOfMonth(currentMonth);
     const monthEnd = dateFns.endOfMonth(monthStart);
     const startDate = dateFns.startOfWeek(monthStart);
     const endDate = dateFns.endOfWeek(monthEnd);
 
     const dateFormat = "D";
+    const eventDateFormat = "YYYY-MM-DD";
     const rows = [];
-
     let days = [];
     let day = startDate;
     let formattedDate = "";
-
+    let eventFormattedDate = "";
     while (day <= endDate) {
       for (let i = 0; i < 7; i++) {
         formattedDate = dateFns.format(day, dateFormat);
+        eventFormattedDate = dateFns.format(day, eventDateFormat);
         const cloneDay = day;
         days.push(
           <div
@@ -72,8 +102,14 @@ class Calendar extends React.Component {
                   : ""
             }`}
             key={day}
-            onClick={() => this.onDateClick(dateFns.parse(cloneDay))}
           >
+            {(events[eventFormattedDate] || []).map(event => {
+              return (
+                <div key={event.id} className="event">
+                  {event.Title}
+                </div>
+              );
+            })}
             <span className="number">{formattedDate}</span>
             <span className="bg">{formattedDate}</span>
           </div>
@@ -90,33 +126,47 @@ class Calendar extends React.Component {
     return <div className="body">{rows}</div>;
   }
 
-  onDateClick = day => {
-    this.setState({
-      selectedDate: day
-    });
-  };
-
   nextMonth = () => {
-    this.setState({
-      currentMonth: dateFns.addMonths(this.state.currentMonth, 1)
-    });
+    let currentMonth = dateFns.addMonths(this.state.currentMonth, 1);
+    const monthStart = dateFns.startOfMonth(currentMonth);
+    const monthEnd = dateFns.endOfMonth(monthStart);
+    const startDate = dateFns.startOfWeek(monthStart);
+    const endDate = dateFns.endOfWeek(monthEnd);
+    const dateFormat = "YYYY-MM-DD";
+    const formattedStartDate = dateFns.format(startDate, dateFormat);
+    const formattedEndDate = dateFns.format(endDate, dateFormat);
+    axios
+      .get(
+        `/events.json?start_date=${formattedStartDate}&end_date=${formattedEndDate}`
+      )
+      .then(response => {
+        this.setState({ currentMonth, events: response.data });
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
   };
 
   prevMonth = () => {
-    this.setState({
-      currentMonth: dateFns.subMonths(this.state.currentMonth, 1)
-    });
+    let currentMonth = dateFns.subMonths(this.state.currentMonth, 1);
+    const monthStart = dateFns.startOfMonth(currentMonth);
+    const monthEnd = dateFns.endOfMonth(monthStart);
+    const startDate = dateFns.startOfWeek(monthStart);
+    const endDate = dateFns.endOfWeek(monthEnd);
+    const dateFormat = "YYYY-MM-DD";
+    const formattedStartDate = dateFns.format(startDate, dateFormat);
+    const formattedEndDate = dateFns.format(endDate, dateFormat);
+    axios
+      .get(
+        `/events.json?start_date=${formattedStartDate}&end_date=${formattedEndDate}`
+      )
+      .then(response => {
+        this.setState({ currentMonth, events: response.data });
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
   };
-
-  render() {
-    return (
-      <div className="calendar">
-        {this.renderHeader()}
-        {this.renderDays()}
-        {this.renderCells()}
-      </div>
-    );
-  }
 }
 
 export default Calendar;
